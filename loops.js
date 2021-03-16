266b76ea0d31df30475f21d7ae6b017585d464dc
Sophie Shin <sophie@alloy.co>
2021-03-16 15:17:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,7 +11,8 @@ function forLoop(array) {
 function whileLoop(n){
     while (n > 0) {
     console.log(--n)
-  }return 'done'}
+  }return 'done'
+}
 
   function doWhileLoop(num){
     function incrementVariable() {
